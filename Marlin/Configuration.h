afa03eb72141f0b67cb9326e5bbd186177cb6ca0
Thomas <itsthetom@googlemail.com>
2021-12-11 12:27:34 +0000

    Revert auto bed leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1384,7 +1384,7 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 215
+#define X_BED_SIZE 213
 #define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
